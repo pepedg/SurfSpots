aadc598a13a7d124e108ab2999a604d4870ea3f0
José Díaz <73610677+Pp-wveDev@users.noreply.github.com>
2020-12-09 03:27:15 +0100

    Added OpenAPI to urls

--- SurfSpotsApp/SurfSpotsApp/urls.py
+++ SurfSpotsApp/SurfSpotsApp/urls.py
@@ -14,8 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import permissions
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+# Configuring drf_yasg
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Graffiti Web",
+      default_version='v1',
+      description="Looking for a spot with awesome waves? You are at the right spot!",
+      terms_of_service="https://www.grafittiApp.com/policies/terms/",
+      contact=openapi.Contact(email="contact@graffitiApp.local"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Apps
+    path('', include('SurfSpotsAPI.urls')),
+    
+    # OpenAPI
+    path('', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name="schema-swagger-ui"),
 ]
